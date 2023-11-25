a57bfb690adee35cf7a231d8addad6ef2f96dcff
slbotbm <rimukshkansal@gmail.com>
2023-11-25 16:15:45 +0900

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,5 +64,16 @@ if coffee:
 if cola:
     st.write("Here you go 🥤")
 
-st.header("st.secrets")
-st.write(st.secrets["message"])
+st.title("st.file_uploader")
+
+st.subheader("Input CSV")
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.subheader("Dataframe")
+    st.write(df)
+    st.subheader("Descriptive statistics")
+    st.write(df.describe())
+else:
+    st.info("Upload a CSV file")
