7ac9f93ab35e183eca35e2961bc9e9335672f940
slbotbm <rimukshkansal@gmail.com>
2023-11-25 16:32:49 +0900

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ st.write(st.experimental_get_query_params())
 st.header("3. Retrieving and displaying information from the URL")
 
 
-firstname = st.experimental_get_query_params()["firstname"]
-surname = st.experimental_get_query_params()["surname"]
+firstname = st.experimental_get_query_params()["firstname"][0]
+surname = st.experimental_get_query_params()["surname"][0]
 
 
 st.write(f"Hello **{firstname} {surname}**, how are you?")
