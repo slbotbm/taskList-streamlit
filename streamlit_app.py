bae3edd816b6014207264b0e53ca1db34870e633
slbotbm <rimukshkansal@gmail.com>
2023-11-25 15:51:06 +0900

    Changed streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,8 @@ st.header("Line chart")
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
+
+st.header("st.selectbox")
+option = st.selectbox("What is your favourite color?", ("Blue", "Red", "Green"))
+
+st.write("Your favourite color is", option)
