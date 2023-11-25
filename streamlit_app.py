2b835a0cf45dc9f4a6fe10ddc7b50abef5414ae8
slbotbm <rimukshkansal@gmail.com>
2023-11-25 15:54:55 +0900

    Changed streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,20 @@ options = st.mutliselect(
 )
 
 st.write("You selected:", options)
+
+st.header("st.checkbox")
+
+st.write("What would you like to order?")
+
+icecream = st.checkbox("Ice cream")
+coffee = st.checkbox("Coffee")
+cola = st.checkbox("Cola")
+
+if icecream:
+    st.write("Great! Here's some more 🍦")
+
+if coffee:
+    st.write("Okay, here's some coffee ☕")
+
+if cola:
+    st.write("Here you go 🥤")
