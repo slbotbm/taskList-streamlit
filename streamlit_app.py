33b1b8030cfa3fbc01cac5e0282ed188f105dffb
slbotbm <rimukshkansal@gmail.com>
2023-11-25 16:30:35 +0900

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,31 @@
 import streamlit as st
-import time
 
-st.title("st.progress")
+st.title("st.experimental_get_query_params")
 
 with st.expander("About this app"):
     st.write(
-        "You can now display the progress of your calculations in a Streamlit app with the `st.progress` command."
+        "`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser."
     )
 
-my_bar = st.progress(0)
+st.header("1. Instructions")
+st.markdown(
+    """
+In the above URL bar of your internet browser, append the following:
+`?firstname=Jack&surname=Beanstalk`
+after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
+such that it becomes 
+`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
+"""
+)
 
-for percent_complete in range(100):
-    time.sleep(0.05)
-    my_bar.progress(percent_complete + 1)
+st.header("2. Contents of st.experimental_get_query_params")
+st.write(st.experimental_get_query_params())
 
-st.balloons()
+st.header("3. Retrieving and displaying information from the URL")
+
+
+firstname = st.experimental_get_query_params()["firstname"][0]
+surname = st.experimental_get_query_params()["surname"][0]
+
+
+st.write(f"Hello **{firstname} {surname}**, how are you?")
